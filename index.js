92bb59e30d80c3312f82571d6a9ab0743082b9ff
Kacper Turon <kacperturon@gmail.com>
2023-05-22 18:00:36 +0100

    first working version

---
+++ index.js
@@ -0,0 +1,48 @@
+const server = "http://127.0.0.1:5000";
+const imgContainer = document.getElementById("picImg");
+const takePicBtnContainer = document.getElementById("takePicBtn");
+const picIdInput = document.getElementById("2020640437");
+const form = document.getElementById("bootstrapForm"); 
+
+console.log(imgContainer);
+console.log(takePicBtnContainer);
+
+const serverOk = async () => {
+  try{
+    const resp = await fetch(`${server}/ping`, {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    }
+      );
+    const ok = await resp.text();
+    return ok === "pong";
+  }catch(e){
+    return false;
+  }
+}
+
+async function takePicture(){
+  const resp = await fetch(`${server}/pic`);
+  const {id, url} = await resp.json();
+  console.log(id, url);
+  imgContainer.setAttribute('src', url);
+  picIdInput.value = id;
+}
+
+async function movePicture(picId){
+  const resp = await fetch(`${server}/pic/${picId}/move`);
+  console.log('moved pic' , resp.status);
+
+}
+
+(async()=>{
+  
+  let serverChecker = setInterval(async()=>{
+    if(await serverOk()){
+      console.log('server ok');
+      clearInterval(serverChecker);
+      takePicBtnContainer.disabled = false;
+
+    }
+  }, 1000);
+})();
--- rpi/snap.py
+++ rpi/snap.py
@@ -1,17 +1,18 @@
 import boto3
 import cv2
 import time
-from flask import Flask
+from flask import Flask, jsonify, Response
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 CAM_PORT = 0
-CAM = cv2.VideoCapture(CAM_PORT)
 CLIENT_S3 = boto3.client('s3', region_name='eu-central-1')
 BUCKET_S3 = 'microscope-grain'
+PICTURES_TEMP_FOLDER = 'temp'
 PICTURES_FOLDER = 'pictures'
 BUCKET_ROOT_URL_S3 = 'https://microscope-grain.s3.eu-central-1.amazonaws.com'
+S3 = boto3.resource('s3')
 
 
 @app.route("/ping")
@@ -24,15 +25,32 @@ def upload_logs_S3():
     pass
 
 
-@app.route("/upload/pic")
+@app.route("/pic")
 def upload_pic_S3() -> str:
     (image_name, file_location) = take_pic()
     url = upload_file_S3(file_location, image_name)
-    return url
+    return jsonify(
+        id=image_name,
+        url=url
+    )
+
+
+@app.route("/pic/<pic_id>/move")
+def move_pic_S3(pic_id) -> str:
+    copy_source = {
+        'Bucket': BUCKET_S3,
+        'Key': f"{PICTURES_TEMP_FOLDER}/{pic_id}"
+    }
+    S3.meta.client.copy(copy_source, BUCKET_S3, f"{PICTURES_FOLDER}/{pic_id}")
+    S3.meta.client.delete_object(
+        Bucket=copy_source["Bucket"], Key=copy_source["Key"])
+    return "ok"
 
 
 def take_pic(pictures_folder=PICTURES_FOLDER) -> tuple[str, str] | None:
+    CAM = cv2.VideoCapture(CAM_PORT)
     result, image = CAM.read()
+    CAM = None
     if result:
         time_str = time.strftime("%Y%m%d-%H%M%S")
         image_name = f"{time_str}.jpg"
@@ -53,7 +71,7 @@ def upload_file_S3(file_location, filename, bucket=BUCKET_S3) -> None:
     print(file_location)
     print(filename)
     uploaded = CLIENT_S3.upload_file(
-        file_location, bucket, f"{PICTURES_FOLDER}/{filename}")
+        file_location, bucket, f"{PICTURES_TEMP_FOLDER}/{filename}")
     # client.upload_file('images/image_0.jpg', 'mybucket', 'image_0.jpg')
 
     # if not uploaded:
@@ -61,4 +79,4 @@ def upload_file_S3(file_location, filename, bucket=BUCKET_S3) -> None:
     #     # TODO: add logging here
     #     pass
     # else:
-    return f"{BUCKET_ROOT_URL_S3}/{PICTURES_FOLDER}/{filename}"
+    return f"{BUCKET_ROOT_URL_S3}/{PICTURES_TEMP_FOLDER}/{filename}"
