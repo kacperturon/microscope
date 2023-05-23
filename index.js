5ee2a1bcc4659ece8a784bf5218d060ff772d211
Kacper Turon <kacperturon@gmail.com>
2023-05-23 22:47:58 +0100

    moved from jquery to pure js

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const takePicBtnContainer = document.getElementById("takePicBtn");
 const picIdInput = document.getElementById("2020640437");
 const form = document.getElementById("bootstrapForm"); 
 
-console.log(imgContainer);
-console.log(takePicBtnContainer);
-
 const serverOk = async () => {
   try{
     const resp = await fetch(`${server}/ping`, {
-      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      method: "GET", 
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
     }
       );
@@ -24,7 +21,6 @@ const serverOk = async () => {
 async function takePicture(){
   const resp = await fetch(`${server}/pic`);
   const {id, url} = await resp.json();
-  console.log(id, url);
   imgContainer.setAttribute('src', url);
   imgContainer.classList = 'pb-2';
   picIdInput.value = id;
@@ -32,18 +28,14 @@ async function takePicture(){
 
 async function movePicture(picId){
   const resp = await fetch(`${server}/pic/${picId}/move`);
-  console.log('moved pic' , resp.status);
-
 }
 
-(async()=>{
-  
-  let serverChecker = setInterval(async()=>{
-    if(await serverOk()){
-      console.log('server ok');
-      clearInterval(serverChecker);
-      takePicBtnContainer.disabled = false;
-
-    }
-  }, 1000);
-})();
+// (async()=>{
+//   let serverChecker = setInterval(async()=>{
+//     if(await serverOk()){
+//       console.log('server ok');
+//       clearInterval(serverChecker);
+//       takePicBtnContainer.disabled = false;
+//     }
+//   }, 1000);
+// })();
