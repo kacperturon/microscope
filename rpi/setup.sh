d45d041f84729d08c57ecdfbda0422ac16720e96
Kacper Turon <kacperturon@gmail.com>
2023-06-11 19:20:12 +0100

    ngrok conf

--- rpi/setup.sh
+++ rpi/setup.sh
@@ -4,7 +4,7 @@
 # cd microscope/rpi
 python -m pip install --upgrade pip
 apt-get update
-apt-get -y install libatlas-base-dev
+sudo apt-get -y install libatlas-base-dev
 python -m pip install --no-cache-dir --prefer-binary -r requirements.txt
 mkdir -p ./pictures
 mkdir -p ~/.aws
@@ -14,7 +14,9 @@ wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.zip
 unzip ngrok-v3-stable-linux-arm.zip
 sudo cp ngrok /usr/bin/ngrok
 cd ~/microscope/rpi
-# ngrok auth
+# cat ngrok.auth >> ~/.config/ngrok/ngrok.yml
+# ngrok authtoken {bitwarden-ngrok-token}
 # echo "aws credentials" >> ~/.aws/credentials
+# killall ngrok
 
 # @reboot sh ~/microscope/rpi/startup.sh
--- rpi/startup.sh
+++ rpi/startup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-ngrok tcp 22
-ngrok http 500
+nohup ngrok start --all &
 nohup python -m flask --app ~/microscope/rpi/snap run --host=0.0.0.0 2>&1 &
